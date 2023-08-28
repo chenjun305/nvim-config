6b8c0d98c8411067daa228f34204bfab14bd11d6
jun.chen <chenjun835@163.com>
2023-08-28 23:03:16 +0800

    my customize

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.winbar = "%=%m %f"
+-- vim.opt.shiftwidth = 2 -- Size of an indent
+-- vim.opt.tabstop = 2 -- Number of spaces tabs count for
